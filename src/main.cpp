7e24610eff2379eaea0ed287af2ce64ce5e51f74
tasmanx <m.kuncevicius@gmail.com>
2020-04-24 12:33:28 +0300

    Add user input

--- src/main.cpp
+++ src/main.cpp
@@ -52,15 +52,16 @@ int main(int argc, const char **argv)
             osm_data = std::move(*data);
     }
     
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
-
+    // User input start & end point coordinates. For example: (10, 10, 90, 90)
+    float start_x, start_y, end_x, end_y;
+    std::cout << "Enter start point x, y coordinates(0-100) & end point x, y coordinates(0-100) (for example: 10, 10, 90, 90):" << "\n";
+    std::cin >> start_x >> start_y >> end_x >> end_y;
+  
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
